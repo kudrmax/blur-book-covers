bd6bc24133f2be0ebb902a474933294f296556ed
kudr.max <mdkudryashov@edu.hse.ru>
2023-11-03 11:12:18 +0300

    Almost working

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import sys
 from PIL import Image, ImageFilter
 
-OriImage = Image.open('input.jpeg')
-gaussImage = OriImage.filter(ImageFilter.GaussianBlur(5))
-# OriImage.show()
-# gaussImage.show()
+input_image = Image.open('input.jpeg')
+blur_image = input_image.filter(ImageFilter.GaussianBlur(5))
 
-gaussImage.save('output.jpg')
+# blur_image.save('output.jpg')
 
-images = [Image.open(x) for x in ['input.jpeg', 'output.jpg']]
-widths, heights = zip(*(i.size for i in images))
+# images = [Image.open(x) for x in ['input.jpeg', 'output.jpg']]
+input_width, input_height = list(input_image.size)
 
-total_width = sum(widths)
-max_height = max(heights)
+output_height = int(input_height)
+output_width = int(16 * output_height / 9)
 
-cover_width = 1920
-cover_height = 1080
+output_image = Image.new('RGB', (output_width, output_height))
 
-new_im = Image.new('RGB', (cover_width, cover_height))
+multiplier = output_width / input_width
+new_size = (int(blur_image.size[0] * multiplier), int(blur_image.size[1] * multiplier))
+output_image.paste(blur_image.resize(new_size), (0, -500))
+# output_image.paste(blur_image_resized, (int(output_width / 2), 0))
+output_image.paste(input_image, (int(output_width / 2 - input_width / 2), 0))
 
-x_offset = 0
-for im in images:
-  new_im.paste(im, (x_offset,0))
-  x_offset += im.size[0]
+# x_offset = 0
+# for im in images:
+#     new_im.paste(im, (300, 0))
+#     x_offset += im.size[0]
 
-new_im.save('merged.jpg')
+output_image.save('merged.jpg')
