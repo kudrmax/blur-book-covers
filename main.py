d7aee0e9c717d04b9aad7d20e5508709717f6222
kudr.max <mdkudryashov@edu.hse.ru>
2023-11-03 10:28:51 +0300

    Created merging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from PIL import Image, ImageFilter
 
 OriImage = Image.open('input.jpeg')
@@ -6,3 +7,21 @@ gaussImage = OriImage.filter(ImageFilter.GaussianBlur(5))
 # gaussImage.show()
 
 gaussImage.save('output.jpg')
+
+images = [Image.open(x) for x in ['input.jpeg', 'output.jpg']]
+widths, heights = zip(*(i.size for i in images))
+
+total_width = sum(widths)
+max_height = max(heights)
+
+cover_width = 1920
+cover_height = 1080
+
+new_im = Image.new('RGB', (cover_width, cover_height))
+
+x_offset = 0
+for im in images:
+  new_im.paste(im, (x_offset,0))
+  x_offset += im.size[0]
+
+new_im.save('merged.jpg')
