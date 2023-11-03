8e4f87abe838dbdf7404c57fddab10f048fdd9bb
kudr.max <mdkudryashov@edu.hse.ru>
2023-11-03 11:49:54 +0300

    Refactored and added examples

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from PIL import Image, ImageFilter
+import glob
+from pathlib import Path
 
-in_image = Image.open('input.jpeg')  # исходная картинка
-blur_image = in_image.filter(ImageFilter.GaussianBlur(10))  # блюрим картинку
+##### МЕНЯТЬ ЭТО #####
+blur_ratio = 20  # коэффициент размытия
+input_image_directory = './images/'  # путь к папке с исходными изображениями
+output_image_directory = './images/output/'  # путь к папке с заблюренными изображениями (должен отличаться от input_image_directory!)
 
-in_width, in_height = in_image.size  # размеры исходной картинки
-out_height, out_width = in_height, int(16 * in_height / 9)  # размеры того, что должно у нас получится на выходе
+##### АЛГОРИТМ #####
+for extension in ['jpg', 'jpeg', 'png']:
+    for in_name in glob.glob(input_image_directory + '*.' + extension):
+        in_image = Image.open(in_name)  # исходная картинка
+        blured_image = in_image.filter(ImageFilter.GaussianBlur(blur_ratio))  # блюрим картинку
 
-multiplier = out_width / in_width  # коэфициент растяжения заблюренной картинки до итоговой картинки
-new_size = (int(blur_image.size[0] * multiplier), int(blur_image.size[1] * multiplier))  # растягиваем
-blur_image = blur_image.resize(new_size)  # растягиваем
+        in_width, in_height = in_image.size  # размеры исходной картинки
+        out_height, out_width = in_height, int(16 * in_height / 9)  # размеры того, что должно у нас получится на выходе
 
-out_image = Image.new('RGB', (out_width, out_height))  # пустое итоговое изображение
-out_image.paste(blur_image, (0, int(- blur_image.size[0] / 2)))  # вставляем заблюренную картинку в итоговую
-out_image.paste(in_image, (int(out_width / 2 - in_width / 2), 0))  # вставляем оригинальную картинку в итоговую
+        multiplier = out_width / in_width  # коэфициент растяжения заблюренной картинки до итоговой картинки
+        new_size = (int(blured_image.size[0] * multiplier), int(blured_image.size[1] * multiplier))  # растягиваем
+        blured_image = blured_image.resize(new_size)  # растягиваем
 
-out_image.save('merged.jpg')  # сохраняем итоговое изображение
+        out_image = Image.new('RGB', (out_width, out_height))  # пустое итоговое изображение
+        out_image.paste(blured_image, (0, int(- blured_image.size[0] / 2)))  # вставляем заблюренную картинку в итоговую
+        out_image.paste(in_image, (int(out_width / 2 - in_width / 2), 0))  # вставляем оригинальную картинку в итоговую
+
+        out_image.save(output_image_directory + Path(in_name).stem + '_blured.jpg')  # сохраняем итоговое изображение
