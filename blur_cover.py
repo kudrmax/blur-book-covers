a2aff340dd3ca012017b83b09071dde6e3fdc339
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-02 17:58:24 +0300

    Fixed pasting small images

--- blur_cover.py
+++ blur_cover.py
@@ -6,7 +6,7 @@ import pyperclip
 
 ##### ИЗМЕНЯТЬ ЭТО #####
 blur_ratio = 20  # коэффициент размытия
-output_image_proportions = 16, 9  # пропорции итогового изображение
+aspect_ratio_x, aspect_ratio_y = 16, 9  # пропорции итогового изображение
 input_image_directory = './images/input/'  # путь к существующей (!) папке с исходными изображениями
 output_image_directory = './images/output/'  # путь к существующей (!) папке с заблюренными изображениями, который отличается от input_image_directory (!)
 archive_image_directory = './images/archive_input/'  # путь к существующей (!) папке, в которую будут отправляться НЕ заблюренные изображение, которые уже заблюрили
@@ -19,14 +19,14 @@ for extension in ['jpg', 'jpeg', 'png', 'webp']:
         blured_image = in_image.filter(ImageFilter.GaussianBlur(blur_ratio))  # блюрим картинку
 
         in_width, in_height = in_image.size  # размеры исходной картинки
-        out_height, out_width = in_height, int(output_image_proportions[0] * in_height / output_image_proportions[1])  # размеры того, что должно у нас получится на выходе
+        out_height, out_width = in_height, int(aspect_ratio_x * in_height / aspect_ratio_y)  # размеры того, что должно у нас получится на выходе
 
         multiplier = out_width / in_width  # коэфициент растяжения заблюренной картинки до итоговой картинки
         new_size = (int(blured_image.size[0] * multiplier), int(blured_image.size[1] * multiplier))  # растягиваем
         blured_image = blured_image.resize(new_size)  # растягиваем
 
         out_image = Image.new('RGB', (out_width, out_height))  # пустое итоговое изображение
-        out_image.paste(blured_image, (0, int(-blured_image.size[0] / 2)))  # вставляем заблюренную картинку в итоговую
+        out_image.paste(blured_image, (0, int(-out_height / 2)))  # вставляем заблюренную картинку в итоговую # ранее: out_image.paste(blured_image, (0, int(-blured_image.size[0] / 2)))
         out_image.paste(in_image, (int(out_width / 2 - in_width / 2), 0))  # вставляем оригинальную картинку в итоговую
 
         out_image.save(output_image_directory + Path(in_name).stem + '.jpg')  # сохраняем итоговое изображение
